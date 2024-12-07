80d65bfbb981f5549cbeb131d0710c70e0f1ae92
Aarayn10-sudo <lazyfox916@gmail.com>
2024-12-07 18:18:20 +0545

    "Updated Navbar"

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,156 +1,179 @@
 "use client";
-
 import Bell from "@/ui/Bell";
 import Cart from "@/ui/Cart";
-import Facebook from "@/ui/Facebook";
-import Instagram from "@/ui/Instagram";
-import Phone from "@/ui/Phone";
-import Twitter from "@/ui/Twitter";
 import Link from "next/link";
+import { useState } from "react";
 
-export default function Navbar() {
+export const navLists = [
+  { links: "/", label: "Home" },
+  { links: "/about", label: "About" },
+  { links: "/products", label: "Products" },
+  { links: "/contact", label: "Contact" },
+];
+
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <>
-      <div className="h-[40px] bg-gray-50 md:px-[30px] px-[10px] flex justify-between items-center  rounded-bl-xl rounded-br-xl">
-        <span className="sm:text-[20px] text-[15px] flex items-center ">
-          <Phone /> : <p>+977 9761627657</p>
-        </span>
-        <div className="flex items-center gap-2 lg:text-[20px] text-[15px]">
-          <p className="">Follow us on : </p>
-          <span className="flex gap-5">
-            <Facebook />
-            <Instagram />
-            <Twitter />
-          </span>
-        </div>
-      </div>
-      <header className="sm:px-3 bg-white opacity-100 text-gray-800 sticky z-50 top-0 ">
-        <div className="container flex justify-between h-16 mx-auto">
-          <div className="flex gap-[50px] items-center">
-            <div className="flex">
-              <Link
-                rel="noopener noreferrer"
-                href="/"
-                aria-label="Back to homepage"
-                className="flex items-center p-2"
-              >
-                <div className="flex items-center justify-center w-12 h-12 rounded-full bg-violet-600">
+    <nav className=" bg-white shadow-sm dark:bg-gray-800 sticky top-0 z-50">
+      <div className="container px-4 py-3 mx-auto">
+        <div className="flex flex-col md:flex-row md:justify-between md:items-center">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-10 md:gap-0">
+              <Link href="/" className="flex items-center gap-1">
+                <div className="flex items-center justify-center w-10 h-10 rounded-full bg-violet-600">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 32 32"
                     fill="currentColor"
-                    className="flex-shrink-0 w-5 h-5 rounded-full text-gray-50"
+                    className="flex-shrink-0 w-4 h-4 rounded-full text-gray-50"
                   >
                     <path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
                   </svg>
                 </div>
+                <h1 className="font-bold text-[20px]">Nexo Footwears</h1>
               </Link>
-              <h1 className="self-center text-2xl font-semibold">
-                Nexo Footwears
-              </h1>
+
+              {/* Search input on desktop */}
+              <div className="flex items-center">
+                <div className="hidden mx-10 md:block">
+                  <div className="relative">
+                    <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                      <svg
+                        className="w-5 h-5 text-gray-400"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                      >
+                        <path
+                          d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        ></path>
+                      </svg>
+                    </span>
+                    <input
+                      type="text"
+                      className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
+                      placeholder="Search"
+                    />
+                  </div>
+                </div>
+                <span className="flex items-center gap-7 text-gray-500">
+                  <Link href={"/cart"}>
+                    <Cart />
+                  </Link>
+                  <Bell />
+                </span>
+              </div>
             </div>
-            <ul className="items-stretch hidden space-x-3 lg:flex uppercase text-[13px]">
-              <li className="flex">
-                <a
-                  rel="noopener noreferrer"
-                  href="/about"
-                  className="flex items-center px-4 -mb-1  text-violet-600 border-violet-600 border-transparent hover:border-2 hover:border-b-violet-700 transition-all ease-in "
+
+            <div className="flex lg:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+                aria-label="toggle menu"
+              >
+                <svg
+                  className={`w-6 h-6 ${isOpen ? "hidden" : ""}`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
                 >
-                  About
-                </a>
-              </li>
-              <li className="flex">
-                <a
-                  rel="noopener noreferrer"
-                  href="#"
-                  className="flex items-center px-4 -mb-1 border-transparent hover:border-2 hover:border-b-violet-700 transition-all ease-in"
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 8h16M4 16h16"
+                  />
+                </svg>
+                <svg
+                  className={`w-6 h-6 ${!isOpen ? "hidden" : ""}`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
                 >
-                  Products
-                </a>
-              </li>
-              <li className="flex">
-                <a
-                  rel="noopener noreferrer"
-                  href="#"
-                  className="flex items-center px-4 -mb-1 border-transparent hover:border-2 hover:border-b-violet-700 transition-all ease-in"
-                >
-                  Catalogs
-                </a>
-              </li>
-              <li className="flex">
-                <a
-                  rel="noopener noreferrer"
-                  href="#"
-                  className="flex items-center px-4 -mb-1 border-transparent hover:border-2 hover:border-b-violet-700 transition-all ease-in"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
 
-          <div className="flex items-center md:space-x-4">
-            <div className="flex gap-4 text-gray-400 text-[18px] items-center">
-              <Link
-                href={"/cart"}
-                className="hover:text-violet-600 transition-all ease-in-out duration-200 flex items-center"
+          <div
+            className={`absolute inset-x-0 z-20 w-full px-6 py-2 transition-all duration-300 ease-in-out bg-white top-16 dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center ${
+              isOpen
+                ? "translate-x-0 opacity-100"
+                : "opacity-0 -translate-x-full"
+            }`}
+          >
+            {navLists.map((navLink) => (
+              <div
+                className="flex flex-col md:flex-row md:mx-1"
+                key={navLink.label}
               >
-                <Cart />
-                <p className="relative top-[-5px] hover:text-violet-600 text-red-500">
-                  3
-                </p>
-              </Link>
-              <Bell />
-            </div>
-            <div className="relative hidden sm:block">
-              <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-                <button
-                  type="submit"
-                  title="Search"
-                  className="p-1 focus:outline-none focus:ring"
+                <Link
+                  className="my-2 text-sm leading-5 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-violet-600 hover:underline md:mx-4 md:my-0 underline-offset-8 decoration-[2px] decoration"
+                  href={navLink.links}
                 >
-                  <svg
-                    fill="currentColor"
-                    viewBox="0 0 512 512"
-                    className="w-4 h-4 text-gray-800"
-                  >
-                    <path d="M479.6,399.716l-81.084-81.084-62.368-25.767A175.014,175.014,0,0,0,368,192c0-97.047-78.953-176-176-176S16,94.953,16,192,94.953,368,192,368a175.034,175.034,0,0,0,101.619-32.377l25.7,62.2L400.4,478.911a56,56,0,1,0,79.2-79.195ZM48,192c0-79.4,64.6-144,144-144s144,64.6,144,144S271.4,336,192,336,48,271.4,48,192ZM456.971,456.284a24.028,24.028,0,0,1-33.942,0l-76.572-76.572-23.894-57.835L380.4,345.771l76.573,76.572A24.028,24.028,0,0,1,456.971,456.284Z"></path>
-                  </svg>
-                </button>
-              </span>
-              <input
-                type="search"
-                name="Search"
-                placeholder="Search..."
-                className="w-32 py-2 pl-10 text-sm rounded-md sm:w-auto focus:outline-none bg-gray-100 border-2 text-gray-800 focus:bg-gray-50 "
-              />
+                  {navLink.label}
+                </Link>
+              </div>
+            ))}
+
+            <div className="md:block">
+              <Link
+                className="text-sm bg-violet-600 w-[80px] h-[25px] text-center flex items-center justify-center rounded-sm text-white cursor-pointer"
+                href={"/log-in"}
+              >
+                Login
+              </Link>
             </div>
-            <Link
-              href={"/log-in"}
-              type="button"
-              className="hidden px-6 py-2 rounded lg:block bg-violet-600 text-gray-50"
-            >
-              Log in
-            </Link>
+
+            <div className="my-4 md:hidden flex flex-col gap-5 items-center">
+              <div className="relative">
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <svg
+                    className="w-5 h-5 text-gray-400"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                  >
+                    <path
+                      d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></path>
+                  </svg>
+                </span>
+                <input
+                  type="text"
+                  className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
+                  placeholder="Search"
+                />
+              </div>
+
+              <Link
+                className="bg-violet-600 p-2 w-[100px] text-center font-medium text-white rounded-lg"
+                href={"/log-in"}
+              >
+                Log-in
+              </Link>
+            </div>
           </div>
-          <button title="Open menu" type="button" className="p-4 lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6 text-gray-800"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
-          </button>
         </div>
-      </header>
-    </>
+      </div>
+    </nav>
   );
-}
+};
+
+export default NavBar;
